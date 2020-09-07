1531f8d14d76de509f5ec026e74dd4d2571b5b96
Jonas Graae <jonas.graae@icloud.com>
2020-09-07 21:49:59 +0200

    Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import "../styles/styles.scss";
+import MobileMenu from "./modules/MobileMenu";
 
 // alert('FATASS Jonas!!!');
 if(module.hot) {
     module.hot.accept();
 }
+
+let mobileMenu = new MobileMenu(); 
